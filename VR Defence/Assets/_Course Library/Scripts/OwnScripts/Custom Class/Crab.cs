8d1ce00e81161e987b13ad51df75001d01fd0e07
Vidar_Edlund <edlund.vidar@gmail.com>
2023-03-09 21:12:23 +0100

    Crabs added
    Added enemy crabs

--- VR Defence/Assets/_Course Library/Scripts/OwnScripts/Custom Class/Crab.cs
+++ VR Defence/Assets/_Course Library/Scripts/OwnScripts/Custom Class/Crab.cs
@@ -6,6 +6,26 @@ public class Crab : MonoBehaviour
 {
     [SerializeField] Entity entity;
 
+    GameObject playerPos;
+    Rigidbody rB;
+    [SerializeField] float speed = 5;
+    [SerializeField] float chase = 3;
+    void Start()
+    {
+        playerPos = GameObject.Find("XR Rig");
+        rB = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 moveDir = new Vector2(playerPos.transform.position.x - transform.position.x, playerPos.transform.position.z - transform.position.z);
+        float toClose = Vector2.Distance(playerPos.transform.position, transform.position);
+      
+        if (toClose <= chase)
+        {
+            rB.velocity = new Vector3(moveDir.x, rB.velocity.y, moveDir.y).normalized * speed * Time.deltaTime;
+        }
+
+    }
     public void TakeDamage(float _attack)
     {
         entity._hp -= _attack;
