ed31a7421e0dde683ab4f99dc53ba6f92b3b3410
Vidar_Edlund <edlund.vidar@gmail.com>
2023-03-10 11:22:30 +0100

    Tried health
    Tried health gave up

--- VR Defence/Assets/_Course Library/Scripts/OwnScripts/Custom Class/Entity.cs
+++ VR Defence/Assets/_Course Library/Scripts/OwnScripts/Custom Class/Entity.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+
+public enum EntityTypes
+{
+    LongRange,
+    ShortRange
+}
 public class Entity
 {
     public string _name;
     public float _hp;
     public float _speed;
     public float _attack;
-
+    EntityTypes entityTypes;
 
 }
