01820372d4503560e376737eb5542ed52344f8d2
Vidar_Edlund <edlund.vidar@gmail.com>
2023-04-20 22:21:49 +0200

    Tutorial updated

---
+++ VR Defence/Assets/ElevatorPuzzle.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPuzzle : MonoBehaviour
+{
+    private float maxCount;
+    private float count = 0;
+
+
+    [SerializeField] private float startDelay = 5;
+    private float delay = 0;
+    //Reference to the obj target
+    [SerializeField] private GameObject obj;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 endPos;
+    [SerializeField] private Vector3 speed;
+
+    private void Awake()
+    {
+        foreach (var child in transform)
+        {
+            maxCount++;
+        }
+      
+    }
+    private void Start()
+    {
+        Debug.Log(maxCount);
+        Debug.Log("In maxCount");
+    }
+    void Update()
+    {
+       
+        if(count >= maxCount)
+        {
+            delay += Time.deltaTime;
+          if(delay >= startDelay)
+            {
+                obj.transform.position += speed * Time.deltaTime;
+            }
+
+          if(obj.transform.position.y >= endPos.y)
+            {
+                count = 0;
+                delay = 0;
+                obj.transform.position = endPos;
+                foreach (Transform child in transform)
+                {
+                    GameObject.Destroy(child.gameObject);
+
+
+                }
+                delay += Time.deltaTime;
+                if (delay >= startDelay)
+                {
+                    obj.transform.position += speed * Time.deltaTime;
+                    if (obj.transform.position.y <= startPos.y)
+                    {
+                        obj.transform.position = startPos;
+                    }
+                }
+               
+            }
+          
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Puzzle"))
+        {
+            count++;
+            Debug.Log("Count increased");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Puzzle"))
+        {
+            count--;
+            Debug.Log("Count decreased");
+        }
+    }
+
+
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5); 
+    }
+}
