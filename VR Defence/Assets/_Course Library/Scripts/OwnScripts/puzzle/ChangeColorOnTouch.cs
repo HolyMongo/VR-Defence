07d5deea8015824a1e01251988ae7bed032990d2
HolyMongo <pererikviktor@gmail.com>
2023-05-04 22:09:29 +0200

    kinda works

--- VR Defence/Assets/_Course Library/Scripts/OwnScripts/puzzle/ChangeColorOnTouch.cs
+++ VR Defence/Assets/_Course Library/Scripts/OwnScripts/puzzle/ChangeColorOnTouch.cs
@@ -7,13 +7,12 @@ public class ChangeColorOnTouch : MonoBehaviour
     [SerializeField] private List<Material> materials;
     [SerializeField] private MeshRenderer mesh;
     [SerializeField] private int currentIndex;
-    [SerializeField] private string tagName;
+    [SerializeField] private GameObject cubeToChange;
 
 
-    float testTimer = 1;
     void Start()
     {
-        mesh = GetComponent<MeshRenderer>();
+        mesh = cubeToChange.GetComponent<MeshRenderer>();
     }
 
     public void UpdateColor()
@@ -26,52 +25,4 @@ public class ChangeColorOnTouch : MonoBehaviour
         Debug.Log("Test!!");
         mesh.material = materials[currentIndex];
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Test!!");
-        if (collision.collider.CompareTag(tagName))
-        {
-            currentIndex++;
-            if (currentIndex > materials.Count - 1)
-            {
-                currentIndex = 0;
-            }
-            Debug.Log("Test!!");
-            mesh.material = materials[currentIndex];
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Test!! trigger");
-        if (other.gameObject.CompareTag(tagName))
-        {
-            currentIndex++;
-            if (currentIndex > materials.Count - 1)
-            {
-                currentIndex = 0;
-            }
-            Debug.Log("Test!!");
-            mesh.material = materials[currentIndex];
-        }
-    }
-
-    private void Update()
-    {
-        //testTimer -= Time.deltaTime;
-
-        //if (testTimer < 0)
-        //{
-        //    testTimer = 3;
-        //    currentIndex++;
-        //    if (currentIndex > materials.Count - 1)
-        //    {
-        //        currentIndex = 0;
-        //    }
-        //    Debug.Log("updated material");
-        //    mesh.material = materials[currentIndex];
-        //}
-    }
-
 }
