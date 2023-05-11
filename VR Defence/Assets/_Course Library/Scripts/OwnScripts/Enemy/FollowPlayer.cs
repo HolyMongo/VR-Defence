7f83cb7533b3a1e5bee98362ab68274eb9a43b92
Vidar_Edlund <edlund.vidar@gmail.com>
2023-05-11 16:46:26 +0200

    Here

--- VR Defence/Assets/_Course Library/Scripts/OwnScripts/Enemy/FollowPlayer.cs
+++ VR Defence/Assets/_Course Library/Scripts/OwnScripts/Enemy/FollowPlayer.cs
@@ -10,6 +10,14 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] float speed = 5;
     [SerializeField] float chase = 3;
     private bool isChasing = false;
+
+
+    //Wander
+    //private bool isWandering = false;
+    //private bool isRotatingLeft = false;
+    //private bool isRotatingRight = false;
+    //private bool isWalking = false;
+
     void Start()
     {
         playerPos = GameObject.Find("XR Rig");
@@ -33,7 +41,7 @@ public class FollowPlayer : MonoBehaviour
     {
         if (!isChasing)
         {
-        Wander();
+          //  StartCoroutine(Wander());
         }
         ChasePlayer();
 
@@ -62,10 +70,42 @@ public class FollowPlayer : MonoBehaviour
             }
         }
     }
-    public void Wander()
-    {
-    
-    }
+  //  IEnumerator Wander()
+    //{
+    //    int rotationTime = Random.Range(1, 3);
+    //    int rotateWait = Random.Range(1, 3);
+    //    int rotateRange = Random.Range(1, 2);
+    //    int walkWait = Random.Range(1, 3);
+    //    int walkTime = Random.Range(1, 3);
+
+    //    isWandering = true;
+
+    //    yield return new WaitForSeconds(walkWait);
+
+    //    isWalking = true;
+
+    //    yield return new WaitForSeconds(walkTime);
+
+    //    isWalking = false;
+
+    //    yield return new WaitForSeconds(rotateWait);
+
+    //    if(rotateRange == 1)
+    //    {
+    //        isRotatingLeft = true;
+    //        yield return new WaitForSeconds(rotationTime);
+    //        isRotatingLeft = false;
+    //    }
+    //    if (rotateRange == 2)
+    //    {
+    //        isRotatingRight = true;
+    //        yield return new WaitForSeconds(rotationTime);
+    //        isRotatingRight = false;
+    //    }
+
+    //    isWandering = false;
+
+    //}
 
     void OnDrawGizmosSelected()
     {
